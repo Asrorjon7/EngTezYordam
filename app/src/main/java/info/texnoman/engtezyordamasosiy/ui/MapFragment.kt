52eb3c666066019aa87e9140118cab7bc2b73787
Kozimjon <kozimjon0597@gmail.com>
2021-12-20 14:56:31 +0500

    Tez yordam

--- app/src/main/java/info/texnoman/engtezyordamasosiy/ui/MapFragment.kt
+++ app/src/main/java/info/texnoman/engtezyordamasosiy/ui/MapFragment.kt
@@ -1,61 +1,59 @@
 package info.texnoman.engtezyordamasosiy.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import info.texnoman.engtezyordamasosiy.R
 import info.texnoman.engtezyordamasosiy.databinding.FragmentMapBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MapFragment : Fragment(),
     OnMapReadyCallback {
     var _binding: FragmentMapBinding? = null
     val binding get() = _binding!!
-
     lateinit var mapFragment: SupportMapFragment
-
     private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val mapFragment = activity?.supportFragmentManager
+            ?.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-        loadMap()
+        //loadMap()
         return binding.root
     }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val mapFragment = requireActivity().supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
     }
-
-    private fun loadMap() {
+   /* private fun loadMap() {
         mapFragment = (childFragmentManager.findFragmentById(R.id.mapAPI) as SupportMapFragment?)!!
         val fm = childFragmentManager
         val ft = fm.beginTransaction()
         mapFragment = SupportMapFragment.newInstance()
         ft.replace(R.id.mapAPI, mapFragment).commit()
         mapFragment.getMapAsync(this)
-    }
+    }*/
+
+
 }
